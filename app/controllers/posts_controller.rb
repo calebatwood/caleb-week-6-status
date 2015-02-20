7d4dd276e440fb28394aff317dd5c4ecab06571b
Caleb Atwood <calebwilliamatwood@gmail.com>
2015-02-20 11:05:36 -0700

    Add instance variables to Posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,31 +1,43 @@
 class PostsController < ApplicationController
 
   def index
-
+    @posts = Post.all
   end
 
   def new
-
+    @post = Post.new
   end
 
   def create
-
+    @post = Post.new(post_params)
+      if @post.save
+        redirect_to @post
+      else
+        render :new, notice: "Post was successfully created!"
+      end
   end
 
   def show
-
+    @post = Post.find(params[:id])
   end
 
   def edit
-
+    @post = Post.find(params[:id])
   end
 
   def update
-
+    @post = Post.find(params[:id])
+      if @post.update(post_params)
+        redirect_to @post, notice: "Post was successfully updated!"
+      else
+        render :edit
+      end
   end
 
   def destroy
-
+    @post = Post.find(params[:id])
+    @post.destroy
+      redirect_to posts_path, notice: "Post was successfully deleted!"
   end
 
 
